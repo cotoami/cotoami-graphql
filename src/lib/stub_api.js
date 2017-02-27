fc465eba6748dd17bba2d67affdd5ac30e7cdbcf
tai2 <hello@tai2.net>
2017-02-27 14:45:24 +0900

    GET /stub/api/amishis/email/:email stub.

--- src/lib/stub_api.js
+++ src/lib/stub_api.js
@@ -12,4 +12,15 @@ router.get('/session', (req, res) => {
     });
 });
 
+router.get('/amishis/email/:email', (req, res) => {
+    res.json({
+        updated_at: '2016-12-25 08:34:05',
+        inserted_at: '2016-12-25 08:34:05',
+        id: 7,
+        email: req.params.email,
+        display_name: 'tai2',
+        avatar_url: 'https://secure.gravatar.com/avatar/b7f799d31a76215358b8364a2654c245',
+    });
+});
+
 export default router;
--- test/stub_api.js
+++ test/stub_api.js
@@ -1,5 +1,7 @@
 import request from 'supertest';
+import expect from 'expect.js';
 import app from '../src/app';
+import assert from 'assert';
 
 describe('GET /stub/api/session/', () => {
     it('respond with json', (done) => {
@@ -7,5 +9,17 @@ describe('GET /stub/api/session/', () => {
         .get('/stub/api/session')
         .expect('Content-Type', /json/)
         .expect(200, done);
+    });
+});
+
+describe('GET /stub/api/amishis/email/:email', () => {
+    it('respond with json', () => {
+        request(app)
+        .get('/stub/api/amishis/email/info@tai2.net')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then(res => {
+            expect(res.body).to.have.property('email', 'info@tai2.net');
+        });
     });
 });
